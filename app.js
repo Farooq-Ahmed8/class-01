6c188a88523a78efb5ad8d01c729668009de102c
Farooq Ahmed <03174748082ali@gmail.com>
2025-02-25 14:37:55 +0500

    my seventh commit

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ var eng = +prompt("Enter your English Marks")
 var math = +prompt("Enter your Math Marks")
 var urdu = +prompt("Enter your Urdu Marks")
 var com = +prompt("Enter your Computer Marks")
+
 var studentObtainedMarks = eng + math + urdu + com;
 var totalMarks = 400;
-var percentage = studentObtainedMarks / totalMarks *100;
+var percentage = (studentObtainedMarks / totalMarks) * 100;
 console.log("Your name is:", userName);
-console.log("Your result is:", percentage +"%" )
+console.log("Your Marks are:", studentObtainedMarks);  
+console.log("Your Percentage is:", percentage + "%");
+
+var passingMarks = 200;
+var result = studentObtainedMarks >= passingMarks ? "Pass" : "Fail";
+console.log("Your Result is:", result);
